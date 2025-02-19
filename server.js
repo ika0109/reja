67b2857b1763a359020f2112c9270d0db9a2f0a0
ika0109 <ikhboloy97@gmail.com>
2025-02-19 22:38:33 +0900

    fix:added some logix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -15,12 +15,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", function (req, res) {
-res.end(`<h1 style="background: red">Hello World by Nana</h1>`)
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"})
 });
-app.get("/gift", function (req, res) {
-    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`)
-    });
+
+app.get("/", function (req, res) { 
+    res.render("harid");
+})
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function (){
