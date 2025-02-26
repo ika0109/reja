41064a24a3853f52dfc60b60be84c6feb45b4cf5
ika0109 <ikhboloy97@gmail.com>
2025-02-27 00:07:51 +0900

    feat: MIT B-Task

--- train.js
+++ train.js
@@ -1,3 +1,31 @@
+function countDigits(str) {
+    let count = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] >= '0' && str[i] <= '9') {
+        count++;
+      }
+    }
+    
+    return count;
+  }
+  
+  
+  console.log(countDigits("ad2a54y79wet0sfgb9")); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // MITASK’larni train.js fileda yozasiz!
 
 // yozish tartibingiz:
